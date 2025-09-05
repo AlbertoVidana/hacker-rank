a00bd150427671d665fe795744fcfe8611559359
Alberto Vidana <alberto.vidana@gmail.com>
2025-09-04 18:02:10 -0600

    #ProblemSolving :: I toke like 9 minuts to complete this problem :: #Time 00:09:00 #Java #Array

--- Problem Solving/Breaking the Records/Solution.java
+++ Problem Solving/Breaking the Records/Solution.java
@@ -1,4 +1,3 @@
-
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
---
+++ Problem Solving/Divisible Sum Pairs/Result.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+    // Write your code here
+        int coun = 0, sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                sum = ar.get(i) + ar.get(j);
+                if (sum % k == 0) {
+                    coun++;
+                }
+            }
+        }
+        return coun;
+    }
+
+}
