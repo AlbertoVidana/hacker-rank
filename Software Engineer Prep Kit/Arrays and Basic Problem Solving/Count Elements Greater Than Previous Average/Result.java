4e23335875452594c37d756e60b6577ba8019b61
Alberto Vidana <alberto.vidana@gmail.com>
2025-09-23 03:00:17 -0600

    #SoftwareEngineerPrepKit :: Count Elements Greater Than Previous Average #Java #Time 00:70:00 #Prepare #Arrays #Basic

---
+++ Software Engineer Prep Kit/Arrays and Basic Problem Solving/Count Elements Greater Than Previous Average/Result.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countResponseTimeRegressions' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY responseTimes as parameter.
+     */
+
+    public static int countResponseTimeRegressions(List<Integer> responseTimes) {
+    // Write your code here
+        if (responseTimes.size() < 1) return 0;
+        long sum = responseTimes.get(0);
+        int coun = 0;
+        for (int i = 1; i < responseTimes.size(); i++) {
+            if (sum < (long) responseTimes.get(i) * i) coun++;
+            sum += responseTimes.get(i);
+        }
+        return coun;
+    }
+
+}
